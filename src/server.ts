d566834e1756bbd48a3ecb4cea0028ac9e2e0ae4
Guilherme Deitos <guilherme.cascavel@gmail.com>
2024-08-03 15:39:41 -0300

    Tirando variaveis inutilizadas do server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
-import router from './controllers'
-import express from 'express'
-import {DataAPIEntity} from './entities/dataAPIEntity'
-import { Router, Request, Response } from 'express';
-//Importar variaveis de ambiente
-
+import router from "./controllers";
+import express from "express";
 const app = express();
-
-app.use(express.json())
-
+app.use(express.json());
 app.use(router)
-
-app.listen(8000, () => {
-    console.log('Server is running on port 3333')
-}
-    
-)
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
